c01cca24d7293dac4baf77e5ceb2e6dfe4dfde09
Kristian <10221425@ucn.dk>
2024-08-03 11:22:24 +0200

    Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
 import Hero from "@/components/Hero"
 import Grid from "@/components/Grid"
 import RecentProjects from "@/components/RecentProjects"
-import Approach from "@/components/Approach";
-// import Footer from "@/components/Footer";
+import Approach from "@/components/Approach"
+import Footer from "@/components/Footer"
 import { FloatingNav } from "@/components/ui/FloatingNav"
 import { navItems } from "@/data"
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
         <Approach />
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </main>
   )
